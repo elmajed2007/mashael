887f608da42042bdce75593391cb8132e362fb57
Toqa Elgazzar <toqahosam59@gmail.com>
2024-12-22 21:47:46 +0200

    edit

--- purchase_piv/models/purchase_order.py
+++ purchase_piv/models/purchase_order.py
@@ -24,15 +24,19 @@ class PurchaseOrder(models.Model):
                 rec.env['purchase.piv'].search([('purchase_order_ids', 'in', rec.id)]))
 
 
-    state = fields.Selection([
-        ('draft', 'RFQ'),
-        ('sent', 'RFQ Sent'),
-        ('piv', 'Piv'),
-        ('to approve', 'To Approve'),
-        ('purchase', 'Purchase Order'),
-        ('done', 'Locked'),
-        ('cancel', 'Cancelled')
-    ], string='Status', readonly=True, index=True, copy=False, default='draft', tracking=True)
+    # state = fields.Selection([
+    #     ('draft', 'RFQ'),
+    #     ('sent', 'RFQ Sent'),
+    #     ('piv', 'Piv'),
+    #     ('to approve', 'To Approve'),
+    #     ('purchase', 'Purchase Order'),
+    #     ('done', 'Locked'),
+    #     ('cancel', 'Cancelled')
+    # ], string='Status', readonly=True, index=True, copy=False, default='draft', tracking=True)
+    #
+    state = fields.Selection(selection_add=[ ('piv', 'Piv'),
+        ('to approve', 'To Approve')])
+
 
 
     def create_piv(self):
